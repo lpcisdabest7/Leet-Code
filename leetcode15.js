2b85a711f123ed9710854508e589b37b2be76589
icisme <lephucuong2003@gmail.com>
2024-07-30 16:40:11 +0700

    Ex 18 19

--- leetcode15.js
+++ leetcode15.js
@@ -1,6 +1,6 @@
 // You are climbing a staircase. It takes n steps to reach the top.
 
-// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// Each time you can either climb 1 or 2 or 3 steps. In how many distinct ways can you climb to the top?
 
 // Example 1:
 
@@ -12,37 +12,58 @@
 // Example 2:
 
 // Input: n = 3
-// Output: 3
+// Output: 4
 // Explanation: There are three ways to climb to the top.
 // 1. 1 step + 1 step + 1 step
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
+// 4. 3 steps
 /**
  * @param {number} n
  * @return {number}
  */
-const n = 3;
-// var climbStairs = function (n) {
-//   if (n === 0 || n === 1) {
-//     return 1;
+
+//   let a = 1;
+//   let b = 1;
+//   let c;
+
+//   for (let i = 2; i <= n; i++) {
+//     c = a + b;
+//     a = b;
+//     b = c;
 //   }
-//   return climbStairs(n - 1) + climbStairs(n - 2);
+
+//   return c;
 // };
+// console.log(climbStairs(n));
+
 var climbStairs = function (n) {
   if (n === 0 || n === 1) {
     return 1;
   }
+  if (n < 0) {
+    return 0;
+  }
+  return climbStairs(n - 1) + climbStairs(n - 2) + climbStairs(n - 3);
+};
 
+console.log(climbStairs(5));
+
+const n = 3;
+var climbStairs1 = function (n) {
+  if (n <= 1) {
+    return 1;
+  }
   let a = 1;
   let b = 1;
-  let c;
-
-  for (let i = 2; i <= n; i++) {
-    c = a + b;
+  let c = 2;
+  let d;
+  for (let i = 3; i <= n; i++) {
+    d = a + b + c;
     a = b;
     b = c;
+    c = d;
   }
-
-  return b;
+  return d;
 };
-console.log(climbStairs(n));
+console.log(climbStairs1(3));
--- leetcode17.js
+++ leetcode17.js
@@ -6,6 +6,32 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    
-};
+
+function merge(nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
+}
+
+let nums1 = [0];
+let m = 0;
+let nums2 = [1];
+let n = 1;
+
+merge(nums1, m, nums2, n);
+console.log(nums1);
